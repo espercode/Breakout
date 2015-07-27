47eecd66e645fbdb90e7952476491a1ac8dd53cd
Spencer Atkinson <spenceratkinson@gmail.com>
2015-07-26 18:33:33 -0700

    adds node

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,59 +1,80 @@
 module.exports = function(grunt) {
 
-	grunt.initConfig({
-	  concat: {
-	    build: {
-	      src: ['js/paddle.js', 'js/vector.js', 'js/walls.js', 'js/ball.js', 'js/box.js'],
-	      dest: 'build/scripts.js',
-	    },
-	  },
-	  watch: {
-	    css: {
-	      files: 'css/**/*.less',
-	      tasks: ['less:build', 'postcss'],
-	      options: {
-	        livereload: true,
-	      },
-	    },
-	    js: {
-	      files: 'js/**/*.js',
-	      tasks: ['concat:js'],
-	      options: {
-	        livereload: true,
-	      },
-	    },
-	  },
-	  less: {
-	    build: {
-	      options: {
-	        paths: ["css"]
-	      },
-	      files: {
-	        "build/styles.css": "css/main.less"
-	      }
-	    },
-	  },
-	  postcss: {
-	      options: {
-	        map: true, // inline sourcemaps
+    grunt.initConfig({
+        concat: {
+            js: {
+                src: ['js/paddle.js', 'js/vector.js', 'js/walls.js', 'js/ball.js', 'js/box.js'],
+                dest: 'build/scripts.js',
+            }
+        },
+        watch: {
+            css: {
+                files: 'css/**/*.less',
+                tasks: ['less:build', 'postcss'],
+                options: {
+                    livereload: true,
+                },
+            },
+            js: {
+                files: 'js/**/*.js',
+                tasks: ['concat:js'],
+                options: {
+                    livereload: true,
+                },
+            },
+        },
+        less: {
+            build: {
+                options: {
+                    paths: ["css"]
+                },
+                files: {
+                    "build/styles.css": "css/main.less"
+                }
+            },
+        },
+        postcss: {
+            options: {
+                map: true, // inline sourcemaps
 
-	        processors: [
-	          require('pixrem')(), // add fallbacks for rem units
-	          require('autoprefixer-core')({browsers: 'last 2 versions'}), // add vendor prefixes
-	          require('cssnano')() // minify the result
-	        ]
-	      },
-	      dist: {
-	        src: 'build/*.css'
-	      }
+                processors: [
+                    require('pixrem')(), // add fallbacks for rem units
+                    require('autoprefixer-core')({
+                        browsers: 'last 2 versions'
+                    }), // add vendor prefixes
+                    require('cssnano')() // minify the result
+                ]
+            },
+            dist: {
+                src: 'build/*.css'
+            }
+        },
+	    connect: {
+	        server: {
+	            options: {
+	                port: 9121,
+	                base: {
+						path: 'build',
+      					keepalive: true,
+      					options: {
+							index: 'breakout.html',
+							maxAge: 300000
+						}
+					},
+					open: true,
+					livereload: true
+	            }
+	        }
 	    }
-	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-less');
-	grunt.loadNpmTasks('grunt-postcss');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
+    });
 
-	grunt.registerTask('default', ['watch']);
+grunt.loadNpmTasks('grunt-contrib-concat');
+grunt.loadNpmTasks('grunt-contrib-watch');
+grunt.loadNpmTasks('grunt-contrib-less');
+grunt.loadNpmTasks('grunt-postcss');
+grunt.loadNpmTasks('grunt-contrib-jshint');
+grunt.loadNpmTasks('grunt-contrib-connect');
+
+grunt.registerTask('default', ['connect', 'watch']);
 };
--- js/walls.js
+++ js/walls.js
@@ -7,23 +7,23 @@ var Walls = function(wall_left, wall_top, wall_right) {
 };
 
 Walls.prototype.drawWalls = function(walls) {
-    // this.wall_left.drawBox(this.wall_left.x, this.wall_left.y, this.wall_left.width, this.wall_left.height);
-    // this.wall_top.drawBox(this.wall_top.x, this.wall_top.y, this.wall_top.width, this.wall_top.height);
-    // this.wall_right.drawBox(this.wall_right.x, this.wall_right.y, this.wall_right.width, this.wall_right.height);
+    this.wall_left.drawBox(this.wall_left.x, this.wall_left.y, this.wall_left.width, this.wall_left.height);
+    this.wall_top.drawBox(this.wall_top.x, this.wall_top.y, this.wall_top.width, this.wall_top.height);
+    this.wall_right.drawBox(this.wall_right.x, this.wall_right.y, this.wall_right.width, this.wall_right.height);
 
-    var path = new Path2D();
-    path.moveTo(50,25);
-    path.quadraticCurveTo(25,25,25,62.5);
-    path.quadraticCurveTo(25,100,50,100);
+    // var path = new Path2D();
+    // path.moveTo(50,25); 
+    // path.quadraticCurveTo(25,25,25,62.5);
+    // path.quadraticCurveTo(25,100,50,100);
 
-    path.quadraticCurveTo(50,120,30,125);
+    // path.quadraticCurveTo(50,120,30,125);
     
-    path.quadraticCurveTo(60,120,65,100);
-    path.lineTo(100, 100);
-    path.quadraticCurveTo(125,100,125,62.5);
-    path.quadraticCurveTo(125,25,100,25);
-    path.lineTo(50,25);
-    ctx.stroke(path);
+    // path.quadraticCurveTo(60,120,65,100);
+    // path.lineTo(100, 100);
+    // path.quadraticCurveTo(125,100,125,62.5);
+    // path.quadraticCurveTo(125,25,100,25);
+    // path.lineTo(50,25);
+    // ctx.stroke(path);
 
 }
 
